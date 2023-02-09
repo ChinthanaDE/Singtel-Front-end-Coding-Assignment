652722f2a0e77b482d7ee1ff54bd65332c38fd93
Chinthana Desilva <chinthanadesilva2@gmail.com>
2023-02-09 16:34:23 +0530

    radio button and check box state not change issue  fixed
    add storybook for addon action

--- client-app/src/stories/Checkbox.stories.tsx
+++ client-app/src/stories/Checkbox.stories.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import CheckBox from '../component/CheckBox/CheckBox';
 
 export default {
-    title: 'Components/CheckBox',
-    component: CheckBox,
-    argTypes: {
-      checked: { type: 'boolean' },
-      color: { control: 'color' }
-    },
-  } as ComponentMeta<typeof CheckBox>;
+  title: 'Components/CheckBox',
+  component: CheckBox,
+  argTypes: {
+    checked: { type: 'boolean' },
+    color: { control: 'color' }
+  },
+} as ComponentMeta<typeof CheckBox>;
 
-const Template: ComponentStory<typeof CheckBox> = (args) => <CheckBox {...args} />;
+const Template: ComponentStory<typeof CheckBox> = (args) => {
+  const [checkboxChecked, setCheckboxChecked] = useState(args.checked);
+
+  const handleCheckboxChange = () => {
+    setCheckboxChecked(!checkboxChecked);
+    args.onChange(args.id);
+  };
+
+  return <CheckBox id={args.id} checked={checkboxChecked} onChange={handleCheckboxChange} color={args.color} />;
+};
 
 export const CheckBoxColor = Template.bind({});
 CheckBoxColor.args = {
   id: 1,
   checked: true,
-  onChange: () => {},
+  onChange: action('checkbox-click'),
 };
+
+
 
--- client-app/src/stories/RadioButton.stories.tsx
+++ client-app/src/stories/RadioButton.stories.tsx
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import RadioButton from '../component/RadioButton/RadioButton';
 
 export default {
-    title: 'Components/RadioButton',
-    component: RadioButton,
-    argTypes: {
-      checked: { type: 'boolean' },
-      color: { control: 'color' }
-    },
-  } as ComponentMeta<typeof RadioButton>;
+  title: 'Components/RadioButton',
+  component: RadioButton,
+  argTypes: {
+    checked: { type: 'boolean' },
+    color: { control: 'color' }
+  },
+} as ComponentMeta<typeof RadioButton>;
 
-const Template: ComponentStory<typeof RadioButton> = (args) => <RadioButton {...args} />;
+const Template: ComponentStory<typeof RadioButton> = (args) => {
+  const [radioChecked, setRadioChecked] = useState(args.checked);
+
+  const handleRadioChange = () => {
+    setRadioChecked(!radioChecked);
+    args.onChange(args.id);
+  };
+
+  return <RadioButton id={args.id} checked={radioChecked} onChange={handleRadioChange} color={args.color} />;
+};
 
 export const RadioButtonColor = Template.bind({});
 RadioButtonColor.args = {
   id: 1,
   checked: true,
-  onChange: () => {},
+  onChange: action('radio-click'),
 };
-
